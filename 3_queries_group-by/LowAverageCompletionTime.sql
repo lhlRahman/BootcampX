a844f11a22867da8cc9ac6b73a5351e8acea3845
Abdul Rahman <lhlrahman@gmail.com>
2023-08-26 19:46:03 -0400

    Add Low Average Completion Time query

---
+++ 3_queries_group-by/LowAverageCompletionTime.sql
@@ -0,0 +1,9 @@
+SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration, AVG(assignments.duration)
+FROM students
+JOIN assignment_submissions
+ON students.id = assignment_submissions.student_id
+JOIN assignments
+ON assignments.id = assignment_submissions.assignment_id
+WHERE students.end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration;
