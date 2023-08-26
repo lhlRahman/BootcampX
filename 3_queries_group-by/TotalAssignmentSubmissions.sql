44b71925257046d57c3193ceac824b8815a1841a
Abdul Rahman <lhlrahman@gmail.com>
2023-08-26 19:45:48 -0400

    Add Total Assignment Submissions query

---
+++ 3_queries_group-by/TotalAssignmentSubmissions.sql
@@ -0,0 +1,8 @@
+SELECT cohorts.name as cohort, COUNT(assignment_submissions.*) as total_submissions
+FROM students
+JOIN cohorts
+ON cohort_id = cohorts.id
+JOIN assignment_submissions
+ON student_id = students.id
+GROUP BY cohort
+ORDER BY total_submissions DESC;
