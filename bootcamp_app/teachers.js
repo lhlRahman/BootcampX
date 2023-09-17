d0ada2771ebecddd4bc5495868b2c4b59012e344
Abdul <lhlrahman@gmail.com>
2023-09-17 19:32:35 -0400

    test sql with pg

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: '123',
+  host: 'localhost',
+  database: 'labber'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, COUNT(*) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2]}'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teachers.name;
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+})
+.catch((err) =>{
+  console.error(err);
+});
